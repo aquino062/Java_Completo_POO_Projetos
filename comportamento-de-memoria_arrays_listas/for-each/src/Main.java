9b830b4eab1bf43d85f01d4857068d0e16d3ba4c
willian aquino <willian_wjunior@hotmail.com>
2023-08-10 11:26:41 -0300

    for-each

---
+++ comportamento-de-memoria_arrays_listas/for-each/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] vet = new String[] {"MARIA","BOB","ALEX"};
+
+        for(String a : vet){
+            System.out.println(a);
+        }
+    }
+}
